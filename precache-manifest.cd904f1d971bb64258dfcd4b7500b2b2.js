1caef7f3f2760832e1c35e41a4a5c702975b3007
tree bagas <tree.bagas@gmail.com>
2020-02-13 17:46:22 +0700

    Updates

---
+++ precache-manifest.cd904f1d971bb64258dfcd4b7500b2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1191946dd62a20bd2c25ee604400dfee",
+    "url": "/foodparadise/index.html"
+  },
+  {
+    "revision": "ae01c604641d8b8c5b4e",
+    "url": "/foodparadise/static/css/main.e902630a.chunk.css"
+  },
+  {
+    "revision": "fb1de39f19aac3f0f0e2",
+    "url": "/foodparadise/static/js/2.f1ea6f87.chunk.js"
+  },
+  {
+    "revision": "ae01c604641d8b8c5b4e",
+    "url": "/foodparadise/static/js/main.21e7d04a.chunk.js"
+  },
+  {
+    "revision": "4622411e31e96098825d",
+    "url": "/foodparadise/static/js/runtime~main.4ef196b0.js"
+  },
+  {
+    "revision": "7e422680aea38f4cab0dc5989772ee00",
+    "url": "/foodparadise/static/media/logo.7e422680.png"
+  },
+  {
+    "revision": "6f9ab5d1292e4e80eb21d0b397b52844",
+    "url": "/foodparadise/static/media/main-banner.6f9ab5d1.jpg"
+  }
+]);
